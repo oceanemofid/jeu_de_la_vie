c40f333fec5a5049147ea0269901b03fc2ace2d7
oceanemofid <oceane.mofid@gmail.com>
2021-04-28 17:27:23 +0200

    added initGrille et nbVoisins

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <libsx.h>
+#include <time.h>
 
 #define M 5
 #define N 5
@@ -16,30 +17,21 @@
 */
 //-------------------------------------------------------//
 
-int *evolution(int grille[M][N]){
-    int i, j;
-    int voisins;
-    int res[M][N];
-    int (*p)[N];
-    p = &res;
-
-    for(int l = 0; l<M; l++){
-        for (int m = 0; m<N; m++){
-            res[l][m] = grille[l][m];
+int *initGrille(int largeur, int longueur){
+    int grille[longueur][largeur];
+    int *p = &grille[0][0];
+    for(int i = 0; i<longueur; i++){
+        for(int j = 0; j<largeur; j++){
+            grille[i][j] = rand() &1; 
         }
     }
-   
-    for(i = 0; i<M; i++){
-        for (j = 0; j<N; j++){
-            if(grille[i-1][j-1] == 1){
-                voisins+=1;
-            }
-            if(grille[i-1][j] == 1){
-                voisins+=1;
-            }
-            if(grille[i-1][j+1] == 1){
-                voisins+=1;
-            }
+    return p;
+}
+
+int nbVoisins(int grille[M][N], int i, int j){
+    int voisins = 0;
+
+    if(i == 0 && j!= 0){
             if(grille[i][j-1] == 1){
                 voisins+=1;
             }
@@ -55,21 +47,87 @@ int *evolution(int grille[M][N]){
             if(grille[i+1][j+1] == 1){
                 voisins+=1;
             }
+    }
+    else if(j == 0 && i != 0){
+        if(grille[i-1][j] == 1){
+            voisins+=1;
+        }
+        if(grille[i-1][j+1] == 1){
+            voisins+=1;
+        }
+        if(grille[i][j+1] == 1){
+            voisins+=1;
+        }
+        if(grille[i+1][j] == 1){
+            voisins+=1;
+        }
+        if(grille[i+1][j+1] == 1){
+            voisins+=1;
+        }
 
-            /*if(voisins == 2 || voisins == 3){
-                //survie
-            }*/
+    }
+    else if(i == 0 && j == 0){
+        if(grille[i][j+1] == 1){
+            voisins+=1;
+        }
+        if(grille[i+1][j] == 1){
+            voisins+=1;
+        }
+        if(grille[i+1][j+1] == 1){
+            voisins+=1;
+        }
+    }
+    else {
+        if(grille[i-1][j-1] == 1){
+            voisins+=1;
+        }
+        if(grille[i-1][j] == 1){
+            voisins+=1;
+        }
+        if(grille[i-1][j+1] == 1){
+            voisins+=1;
+        }
+        if(grille[i][j-1] == 1){
+            voisins+=1;
+        }
+        if(grille[i][j+1] == 1){
+            voisins+=1;
+        }
+        if(grille[i+1][j-1] == 1){
+            voisins+=1;
+        }
+        if(grille[i+1][j] == 1){
+            voisins+=1;
+        }
+        if(grille[i+1][j+1] == 1){
+            voisins+=1;
+        }
+    }
+    return voisins;
 
+}
+
+int *evolution(int grille[M][N]){
+    int i, j;
+    int voisins;
+    int res[M][N];
+    int *p = &res[0][0];
+
+    for(int l = 0; l<M; l++){
+        for (int m = 0; m<N; m++){
+            res[l][m] = grille[l][m];
+        }
+    }
+   
+    for(i = 0; i<M; i++){
+        for (j = 0; j<N; j++){
+            voisins = nbVoisins(grille, i, j);
             if(voisins == 3){
                 //naissance
                 res[i][j] = 1;
             }
-            else if(voisins >= 4){
-                //mort par étouffement
-                res[i][j] = 0;
-            }
-            else if (voisins == 0 || voisins == 1){
-                //mort par isolement
+            else if(voisins < 2 || voisins >= 4){
+                //mort par étouffement et par isolement
                 res[i][j] = 0;
             }
         }
@@ -106,12 +164,14 @@ int main(){
                     {0,0,0,0,0}};
     
     //afficherGrille(evolution(test));
-    int *p = evolution(test);
+    int *p;
+    p = evolution(test);
     for(int i = 0; i<M ; i++){
         for (int j = 0; j<N; j++){
-            printf("%2d | ", *(p++));
+            printf("%d | ", *(p++));
         }
         printf("\n");
     }
+    //while
     return EXIT_SUCCESS;
 }
