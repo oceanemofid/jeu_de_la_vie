ef64d47587156d8120c2ed827befbc9467cc4e7e
Océane Mofid <oceane.mofid@gmail.com>
2021-05-26 00:01:54 +0200

    début

--- callbacks.c
+++ callbacks.c
@@ -1,8 +1,6 @@
 /*
- * les callbacks de l’application
+ * les callbacks de l’application, appelle fonctions modele + widget vue
  */
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <libsx.h>
@@ -15,4 +13,58 @@
  */
 void quit(Widget w, void *d) {
     exit(EXIT_SUCCESS);
+}
+
+void generation_grille(){
+    int grille[M][N];
+    //int GetMenuItemChecked(Widget w); -> 1 : checked
+    //selon l'iten checked, aléatoire ou ouverture du fichier
+    //pour fichier conversion du binaire en grille 
+    initGrille(grille);
+    //ouvre fenetre
+    //SetCurrentWindow(Widget w);
+    run_basique(Box, grille);
+    run_variante();
+}
+
+void afficher(int grille[M][N], Widget w){
+    int voisins;
+    for(int i = 0; i<M; i++){
+        for (int j = 0; j<N; j++){
+            voisins = nbVoisins(grille,i,j);
+            if(grille[i][j]== 0 && voisins == 3){
+                //Draw
+            }
+            else if(grille[i][j]== 1 && voisins >= 4){
+                //Draw
+            }
+            else if(grille[i][j]== 1 && voisins <=1){
+                //Draw
+            }
+            else if(grille[i][j]== 0){
+                //Draw
+            }
+            else{
+                //Draw
+            }
+        }
+    }
+}
+
+void run_basique(Widget w, int grille [M][N]){
+    //boucle timer
+    afficher(grille,Box);
+    evolution(grille);
+    //Redisplay
+    SyncDisplay();
+    
+}
+
+void run_variante(){
+    //boucle timer
+    afficher(grille,Box);
+    dayandnight(grille);
+    //Redisplay
+    SyncDisplay();
+    
 }
